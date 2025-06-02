4e5056f0774633b718ef9d4ef29649e488424cdd
mochamadyudi <myudisobari12@gmail.com>
2025-06-02 15:58:52 +0700

    test

--- pkg/core/interfaces/sunda-factory.interface.ts
+++ pkg/core/interfaces/sunda-factory.interface.ts
@@ -1,5 +1,7 @@
 import { SundaApplicationOptions } from '@sundajs/common';
+import { Testing } from '@sundajs/common';
 
 export interface ISundaFactory {
   create<T = any>(module: any, options: SundaApplicationOptions): Promise<T>;
+  microService<T = any>(provider: any): any;
 }
